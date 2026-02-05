7eb8605f068558e8256b0abf80882dda3d0c9221
dNSCoder <wanarase.s@ubu.ac.th>
2026-02-05 18:23:41 +0700

    Initial commit with requirements and gitignore

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/myname/<name>")
+def hello_world(name):
+    return f"Hello, Worlddddddd........! {name}"
+
+@app.route("/myprofile")
+def myprofile():
+    return render_template("my_profile.html", username="Wanarase", age=26, title="My Profile", is_admin = True)
+
+@app.route("/tickets/<int:ticket_id>")
+def ticket_detail(ticket_id):
+    return f"Ticket #{ticket_id}"
+
+@app.route("/")
+def index():
+    return render_template("home.html", title="Home")
+
+
+@app.route("/about")
+def about():
+    print("call about page!!!!!!!!!!!")
+    return render_template("about.html", title="about")
+
+@app.route("/listRequest")
+def list_request():
+    request = [
+    {"ids": 1, "case_name": "แจ้งซ่อมพัดลม"},
+    {"ids": 2, "case_name": "แจ้งซ่อมแอร์"},
+    {"ids": 3, "case_name": "แจ้งซ่อมประตู"}
+    ]
+    return render_template("request_cases.html", request_cases=request, title = "Case")
+
+@app.route("/new_case")
+def new_case():
+    return render_template("new.html")
+
+# @app.route("/new_case/new", methods=["POST"])
+# def add_new():
+
+
+if __name__ == "__main__":
+  app.run(debug=True, port =8000, host='0.0.0.0')
